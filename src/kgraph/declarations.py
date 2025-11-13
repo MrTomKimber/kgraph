f36b40fe6b1c1c75f9347e38a2e07282a45c00a5
Tom Kimber <tom.kimber@gmail.com>
2025-11-13 18:15:39 +0000

    initial testing setup

--- src/kgraph/declarations.py
+++ src/kgraph/declarations.py
@@ -1,6 +1,7 @@
 """This module defines shared definitions and reference resources 
 to be imported by other modules within the package"""
 
+import os
 from importlib import resources
 from typing import Tuple, TypeAlias, Union
 from rdflib import URIRef, Literal, BNode
@@ -10,7 +11,11 @@ import json
 import kgraph.ontologies, kgraph.jschema
 
 # SERIALISATIONSCHEMA is the json-schema that validates serialisation configuration files
-SERIALISATIONSCHEMA = json.loads(resources.read_text(kgraph.jschema, "serialisationschema.json"))
+with resources.as_file(resources.files(kgraph.jschema) / os.path.normpath("serialisationschema.json")) as serialisation_schema_file:
+    with serialisation_schema_file.open('r', 
+                                        encoding='utf-8',
+                                        errors='strict') as serialisation_schema:
+        SERIALISATIONSCHEMA = json.load(serialisation_schema)
 
 # KGMETA is the fixed namespace used by the kgraph suite of utilities
 # Here the namespace is defined, and the underlying ontology resource
@@ -18,14 +23,16 @@ SERIALISATIONSCHEMA = json.loads(resources.read_text(kgraph.jschema, "serialisat
 # namespaces to the graph's namespace manager.
 KGMETA = Namespace("https://kgraph.foo/onto/kgmeta#")
 KGMETA_G = rdflibGraph()
-with resources.path( kgraph.ontologies, "kgmeta.owl") as kgmeta_owl:
+
+with resources.as_file(resources.files(kgraph.ontologies) / os.path.normpath("kgmeta.owl")) as kgmeta_owl:
     KGMETA_G.parse(kgmeta_owl, format='xml')
     KGMETA_G.bind("KGMETA", KGMETA.title.toPython())
 
 KGMETA_SHAPES_G = rdflibGraph()
-with resources.path( kgraph.ontologies, "kgmeta_shacl.ttl") as kgmeta_shapes:
+with resources.as_file(resources.files(kgraph.ontologies) / os.path.normpath("kgmeta_shacl.ttl")) as kgmeta_shapes:
     KGMETA_SHAPES_G.parse(kgmeta_shapes, format='ttl')
     KGMETA_SHAPES_G.bind("KGMETA", KGMETA.title.toPython())
+
 
 
 # Define the type-aliases used to handle triple contents
